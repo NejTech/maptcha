aa2cf7bfe48b8f25ee05daffdad3c87d7dfd67a3
nejtejan <nejtejan@pc412-21.msad.fel.cvut.cz>
2016-12-22 16:38:01 +0100

    Add proper output formatting

--- matlab_captcha.m
+++ matlab_captcha.m
@@ -12,12 +12,17 @@
 [ light1, light2, dark1, dark2 ] = random_palette;
 
 possible_letters = 'ABCDEFHKLMNPRSTUVXYZabcdekmnrstuvwxyz23456789';
+captcha_code = [ ...
+    possible_letters(round(rand*length(possible_letters))), ...
+    possible_letters(round(rand*length(possible_letters))), ...
+    possible_letters(round(rand*length(possible_letters))), ...
+    possible_letters(round(rand*length(possible_letters)))];
 
 % generate the captcha letters
-ltr1 = gen_char(possible_letters(round(rand*length(possible_letters))), dark1, light1, dark2);
-ltr2 = gen_char(possible_letters(round(rand*length(possible_letters))), light1, dark1, light2);
-ltr3 = gen_char(possible_letters(round(rand*length(possible_letters))), dark2, light2, dark1);
-ltr4 = gen_char(possible_letters(round(rand*length(possible_letters))), light1, dark2, light2);
+ltr1 = gen_char(captcha_code(1), dark1, light1, dark2);
+ltr2 = gen_char(captcha_code(2), light1, dark1, light2);
+ltr3 = gen_char(captcha_code(3), dark2, light2, dark1);
+ltr4 = gen_char(captcha_code(4), light1, dark2, light2);
 
 % make sure they are the same size (thx, MATLAB)
 ltr1 = ltr1(1:85,1:50,1:3);
@@ -25,7 +30,9 @@ ltr2 = ltr2(1:85,1:50,1:3);
 ltr3 = ltr3(1:85,1:50,1:3);
 ltr4 = ltr4(1:85,1:50,1:3);
 
-% concat them to an image
+% concat them to an image and encode it to base64 JPG
 result = [ltr1, ltr2, ltr3, ltr4];
+result_b64 = encode_base64(result);
 
-encode_base64(result)
+% print the code, a newline and the base64 to stdout
+fprintf('%s\n%s\n', captcha_code, result_b64);
